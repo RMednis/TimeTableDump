b980518fce31937d1f7392ab259016dff2f112af
RMednis <mednisreinis@gmail.com>
2017-09-16 01:09:46 +0300

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -5,9 +5,8 @@ wget <url>/index.swf #Replace <url> with your own one.
 if md5sum -c checklist.chk; then #checks for changes
 	exit
 else
-	wait %1 #waits for rm to finish
 	rm timetables/ #removes previous data
-	wait %1 #Waits for it to be done
+	wait %1 #Waits for rm to finish
 	ffdec -export frame timetables/ index.swf #uses ffdec to export all frames
 	md5sum index.swf > checklist.chk #creates md5 checksum to check for changes
 fi
