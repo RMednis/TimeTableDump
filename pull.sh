a45eb4e0424fd1f25cc27df9bb4c145fb2e992fe
RMednis <mednisreinis@gmail.com>
2017-09-16 01:08:13 +0300

    Add files via upload

--- pull.sh
+++ pull.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #  Reinis Gunārs Mednis 2017
-
 rm index.swf #removes previous swf file
-wait %1 #waits for rm to finish
-rm timetables/ #removes previous data
 wget <url>/index.swf #Replace <url> with your own one.
-wait %1 #Waits for it to be done
-ffdec -export frame timetables/ index.swf #uses ffdec to export all frames
+if md5sum -c checklist.chk; then #checks for changes
+	exit
+else
+	wait %1 #waits for rm to finish
+	rm timetables/ #removes previous data
+	wait %1 #Waits for it to be done
+	ffdec -export frame timetables/ index.swf #uses ffdec to export all frames
+	md5sum index.swf > checklist.chk #creates md5 checksum to check for changes
+fi
