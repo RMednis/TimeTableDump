e93add47ce50430d0b42c907538049655266e0c0
RMednis <mednisreinis@gmail.com>
2017-09-16 15:48:39 +0300

    Added git integration

--- pull.sh
+++ pull.sh
@@ -7,6 +7,10 @@ if md5sum -c checklist.chk; then #checks for changes
 else
 	rm timetables/ #removes previous data
 	wait %1 #Waits for rm to finish
-	ffdec -export frame timetables/ index.swf #uses ffdec to export all frames
+	ffdec -export frame <repository>/timetables/ index.swf #uses ffdec to export all frames
 	md5sum index.swf > checklist.chk #creates md5 checksum to check for changes
+	cd <repository>/
+	git commit -m "Extracted images on `date +'%Y-%m-%d %H:%M:%S'`";
+	git push -f origin master;
 fi
+
