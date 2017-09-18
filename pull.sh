47ca0aa869febaf8759c9906ed21c2d708aacc1f
Mednis <mednisreinis@gmail.com>
2017-09-18 22:43:43 +0300

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -10,6 +10,7 @@ else
 	ffdec -export frame <repository>/timetables/ index.swf #uses ffdec to export all frames
 	md5sum index.swf > checklist.chk #creates md5 checksum to check for changes
 	cd <repository>/
+	git add -A
 	git commit -m "Extracted images on `date +'%Y-%m-%d %H:%M:%S'`";
 	git push -f origin master;
 fi
