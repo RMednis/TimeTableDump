5ca91a85cf16c30dd9edf0506d9910778fe6ad8a
Mednis <mednisreinis@gmail.com>
2018-06-16 01:29:34 +0300

    Rewrote parts of the main change pulling script!
    It should be easier to read, and we now have direct veriables, to make customization easy!

--- pull.sh
+++ pull.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
-#  Reinis Gunārs Mednis 2017
-rm index.swf #removes previous swf file
-wget <url>/index.swf #Replace <url> with your own one.
-if md5sum -c checklist.chk; then #checks for changes
+
+######################################
+#     Reinis Gunārs Mednis 2018		 #
+# Student Timetable Dump version 2.3 #
+######################################
+# Please read the README.md file for #
+#         more information!          #
+######################################
+
+# The path where the timetables should be extracted to.
+# Example:
+# PATH="/timetables/"
+PATH=''
+
+
+# The URL where the file is hosted on the server
+# Example:
+# URL='http://ovt.lv/stundas/stundu_saraksts/index.swf'
+URL=''
+
+# Removing the previous files (assuming the file is named index.swf)
+rm index.swf
+
+# Pulling the file from the webserver, saving it as index.swf
+wget $URL -O index.swf
+
+# Checking for changes using md5 hashes!
+if md5sum -c checklist_students.chk; then 
+	# Both old and new hashes match!
 	exit
 else
-	rm timetables/ #removes previous data
-	wait %1 #Waits for rm to finish
-	ffdec -export frame <repository>/timetables/ index.swf #uses ffdec to export all frames
-	md5sum index.swf > checklist.chk #creates md5 checksum to check for changes
-	cd <repository>/
-	git add -A
-	git commit -m "Extracted images on `date +'%Y-%m-%d %H:%M:%S'`";
-	git push -f origin master;
+	# Hashes are diferent, there seem to be changes!
+	rm $PATH # Removes previous timetables / their folder.
+	ffdec -export frame $PATH index.swf # Uses ffdec to export all frames from the swf files.
+	md5sum index.swf > checklist_students.chk # Creates md5 checksum to check for changes.
+	
+	# Git pushing the changes (optional)
+	# cd <repository>/
+	# git add -A
+	# git commit -m "Extracted images on `date +'%Y-%m-%d %H:%M:%S'`";
+	# git push origin master;
 fi
-
